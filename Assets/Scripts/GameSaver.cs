3ed1c370f71c42f586fd103ff58f13685ef7db52
NotRedC <redcharizard1@gmail.com>
2026-01-17 23:25:48 +0200

    SaveGame
    -adaugat salvarea jocului atunci cand se inchide

---
+++ Assets/Scripts/GameSaver.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameSaver : MonoBehaviour
+{
+    private Rigidbody2D body;
+
+    void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        LoadGame();
+    }
+
+    private void OnApplicationQuit()
+    {
+        SaveGame();
+    }
+
+    public void SaveGame()
+    {
+  
+        string currentScene = SceneManager.GetActiveScene().name;
+        PlayerPrefs.SetString("SavedScene", currentScene);
+        PlayerPrefs.SetFloat("PosX", transform.position.x);
+        PlayerPrefs.SetFloat("PosY", transform.position.y);
+        PlayerPrefs.Save();
+
+        Debug.Log("Game Saved!");
+    }
+
+    public void LoadGame()
+    {
+
+        if (!PlayerPrefs.HasKey("PosX")) return;
+
+    
+        float x = PlayerPrefs.GetFloat("PosX");
+        float y = PlayerPrefs.GetFloat("PosY");
+
+        transform.position = new Vector2(x, y);
+        if (body != null) body.linearVelocity = Vector2.zero;
+
+        Debug.Log("Game Loaded at: " + x + ", " + y);
+    }
+}
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,7 +5,15 @@ public class MainMenu : MonoBehaviour
 {
     public void Play() // functia pt play
     {
-        SceneManager.LoadScene(1); // incarca jocul
+        if (PlayerPrefs.HasKey("SavedScene"))
+        {
+            string sceneToLoad = PlayerPrefs.GetString("SavedScene");
+            SceneManager.LoadScene(sceneToLoad);
+        }
+        else
+        {
+            SceneManager.LoadScene("Level_1");
+        }
     }
 
     public void Exit() // functia pt exit
